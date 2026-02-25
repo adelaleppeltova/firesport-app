ad25cf4b33db1c978644bf874b1f1c5d9ed53408
Adéla Leppeltová <83660219+adelaleppeltova@users.noreply.github.com>
2026-02-25 20:17:53 +0100

    add: data

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,12 +9,14 @@ echo "⏳ Čekání na MongoDB..."
 python /app/scripts/wait_for_mongodb.py
 
 # Automatický import dat
-echo "📥 Spouštím import dat..."
-python /app/scripts/load_data.py
+if [ "$IMPORT_DATA" = "true" ]; then
+  echo "📥 Spouštím import dat..."
+  python /app/scripts/load_data.py
+fi
 
 if [ "$DEBUG" = "true" ]; then
   echo "🐞 Debug mode ON"
-  exec python -m debugpy --listen 0.0.0.0:5679 --wait-for-client \
+  exec python -m debugpy --listen 0.0.0.0:5679 \
     -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 else
   exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
